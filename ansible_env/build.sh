ffe101233ed3365d48c3bec47f3d824366d4022e
Satoe Imaishi <simaishi@redhat.com>
2020-10-05 17:47:38 -0400

    Build rpm in copr

--- ansible_env/build.sh
+++ ansible_env/build.sh
@@ -17,4 +17,10 @@ deactivate
 echo "*** Building rpm"
 mkdir -p $rpm_path
 tar -C $VENV_PATH --transform "s,^,$NAME-$VERSION/," -hzcf $rpm_path/$NAME-$VERSION.tar.gz .
-rpmbuild -bb --define "_sourcedir $rpm_path" --define "_rpmdir $rpm_path" /$NAME.spec
+
+if [ -f "/root/.config/copr" ]; then
+  rpmbuild -bs --define "_sourcedir $rpm_path" --define "_srcrpmdir $rpm_path" /$NAME.spec
+  copr-cli build -r epel-8-x86_64 test $rpm_path/*.src.rpm
+else
+  rpmbuild -bb --define "_sourcedir $rpm_path" --define "_rpmdir $rpm_path" /$NAME.spec
+fi
